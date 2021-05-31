7c0d5399812d50ee522db4a733350554efa0e1a2
Jim Verheijde <jim.verheijde@hotmail.com>
2021-06-01 00:05:38 +0200

    Update scripts

--- scripts/deploy-AWS-config.sh
+++ scripts/deploy-AWS-config.sh
@@ -2,10 +2,10 @@
 
 TAR_FILE="hazelcast-jet-4.4.10-SNAPSHOT"
 
-#REMOTE_HOSTS=(amazon1)
-#REMOTE_USERS=(ubuntu)
-REMOTE_HOSTS=(amazon1 amazon2 amazon3)
-REMOTE_USERS=(ubuntu ubuntu ubuntu)
+REMOTE_HOSTS=(amazon1)
+REMOTE_USERS=(ubuntu)
+#REMOTE_HOSTS=(amazon1 amazon2 amazon3)
+#REMOTE_USERS=(ubuntu ubuntu ubuntu)
 #REMOTE_HOSTS=(amazon1 amazon2 amazon3 amazon4 amazon5)
 #REMOTE_USERS=(ubuntu ubuntu ubuntu ubuntu ubuntu)
 #REMOTE_HOSTS=(amazon1 amazon2 amazon3 amazon4 amazon5 amazon6 amazon7)
